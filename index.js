a06e8e96d4d5abe21ee499572294b117811d073d
GJYyyyy <772048440@qq.com>
2024-10-14 17:00:11 +0800

    优化入口文件

--- index.js
+++ index.js
@@ -4,25 +4,3 @@ const FileNameFormat = require('./src/FileNameFormat');
 const fnFormat = new FileNameFormat();
 
 // 具体操作
-const replaces = [
-    {
-        whiteList: [],
-        resFolder: 'D:/gjy/ceyu/jujia_hospital/static2/monitor4/img',
-        codeFolder: "D:/gjy/ceyu/jujia_hospital/static2",
-    },
-    {
-        whiteList: ['custom.scss', 'customElementUI.scss', 'customMonitor.scss'],
-        resFolder: 'D:/gjy/ceyu/jujia_hospital/src/assets',
-        codeFolder: "D:/gjy/ceyu/jujia_hospital/src",
-    },
-    {
-        whiteList: [],
-        resFolder: 'D:/gjy/ceyu/jujia_hospital/static',
-        codeFolder: "D:/gjy/ceyu/jujia_hospital/src",
-    },
-]
-
-let replace = replaces[2];
-
-fnFormat.setResWhiteList(replace.whiteList);
-fnFormat.format(replace.resFolder, replace.codeFolder);
